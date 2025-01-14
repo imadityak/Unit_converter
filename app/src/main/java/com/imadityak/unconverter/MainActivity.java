b2cc448bdb386370e1052e0ed84ce1e72b80a77e
imadityak <adityacoding.one@gmail.com>
2025-01-14 22:31:54 +0530

    kg to pound

--- app/src/main/java/com/imadityak/unconverter/MainActivity.java
+++ app/src/main/java/com/imadityak/unconverter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.imadityak.unconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // connecting the view to the java object
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView_2);
+        editText = findViewById(R.id.editTextNumber);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205 * kg;
+                textView.setText("The corresponding value in pounds is \n" + pound);
+                Toast.makeText(MainActivity.this, "Converted", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
